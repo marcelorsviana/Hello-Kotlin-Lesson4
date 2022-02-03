509a563d5283c5b9c57c996ff54bb2a740aaff23
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-03 00:08:25 -0300

    Task 10 - singletons, enums, and sealed classes

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -32,6 +32,18 @@ fun makeDecorations(){
 // Here is a dat class with 3 properties.
 data class Decoration2(val rocks: String, val wood: String, val diver: String){}
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun main() {
-    makeDecorations()
+//    makeDecorations()
+
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
 }

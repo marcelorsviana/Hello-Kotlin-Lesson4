9ad3930e52b50ed2ab9b38f7f900823594f9348b
Marcelo Viana <marcelorsviana@gmail.com>
2022-02-02 22:42:36 -0300

    Task 7 - Compare abstract classes and interfaces

--- src/main/kotlin/example/myapp/main.kt
+++ src/main/kotlin/example/myapp/main.kt
@@ -39,6 +39,17 @@ fun buildAquarium() {
 
 }
 
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
 fun main() {
-    buildAquarium()
+//    buildAquarium()
+    makeFish()
 }
